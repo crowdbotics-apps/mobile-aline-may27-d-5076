30a468cf64c690e062e23cb8ed88620d7d15043b
Crowdbotics <team@crowdbotics.com>
2020-06-22 16:51:05 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
-    isCelsius = models.BigIntegerField(null=True, blank=True,)
+    isSomething = models.BooleanField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
